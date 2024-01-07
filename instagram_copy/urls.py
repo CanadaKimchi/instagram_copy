8e012f0fafc014fd4c6d982b877e077d17e0d8db
CanadaKimchi <fabianjung228@gmail.com>
2024-01-06 23:03:23 -0500

    inital commit

--- instagram_copy/urls.py
+++ instagram_copy/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from instagram_copy.views import Sub
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Sub.as_view())
+
+
 ]
